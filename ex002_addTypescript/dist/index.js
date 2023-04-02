7c1f9f9b91c4207e52fbc33b742c8eba40b101dd
Luan Cordeiro <luam.lc19@gmail.com>
2023-04-02 12:38:53 -0300

    configure typescript

---
+++ ex002_addTypescript/dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let user = 'Luan';
+let age = 90;
+console.log(`Oi eu sou ${user} e tenho ${age} anos`);
---
+++ ex002_addTypescript/index.ts
@@ -0,0 +1,4 @@
+let user: string = 'Luan';
+let age: number = 90;
+
+console.log(`Oi eu sou ${user} e tenho ${age} anos`);
