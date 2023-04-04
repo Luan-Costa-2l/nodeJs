18ddbfb2a08589f817e05af0da1004ac2fe2328a
Luan Cordeiro <luam.lc19@gmail.com>
2023-04-04 10:49:20 -0300

    scripts package.json

--- ex002_addTypescript/dist/index.js
+++ ex002_addTypescript/dist/index.js
@@ -2,3 +2,4 @@
 let user = 'Luan';
 let age = 90;
 console.log(`Oi eu sou ${user} e tenho ${age} anos`);
+// aviso: use o comando npm run (atalho) para rodar seus escripts, se for um atalho conhecido como o start não precisa do run
--- ex002_addTypescript/index.ts
+++ ex002_addTypescript/index.ts
@@ -2,3 +2,5 @@ let user: string = 'Luan';
 let age: number = 90;
 
 console.log(`Oi eu sou ${user} e tenho ${age} anos`);
+
+// aviso: use o comando npm run (atalho) para rodar seus escripts, se for um atalho conhecido como o start não precisa do run
