a97b0e2c360ab4b5625b98359b30e47a9529d9a4
Luan Cordeiro <luam.lc19@gmail.com>
2023-04-05 10:19:28 -0300

    use nodemon and ts-node

---
+++ ex004_nodemon_tsNode/index.ts
@@ -0,0 +1,8 @@
+// nodemon restart automatically when your code changes
+console.log('i\'m a node project');
+
+// ts-node enabling you to directly execute TypeScript on Node.js without precompiling
+console.log('i\'m a new line.');
+
+// nodemon automatically use ts-node command when its intalled
+console.log('i\'m other a new line.');
