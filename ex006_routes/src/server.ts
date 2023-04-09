dd9aacc73dc8a4a79c76f3ac1e34616e3555fce6
Luan Cordeiro <luam.lc19@gmail.com>
2023-04-09 09:51:16 -0300

    introdução a rotas

---
+++ ex006_routes/src/server.ts
@@ -0,0 +1,31 @@
+import express, { Request, Response} from 'express';
+
+const app = express();
+const port = 3000;
+
+// o get é um metodo como post, put e delete
+app.get('/', (req: Request, res: Response) => {
+    res.end('Hello World!');
+});
+
+// outra forma de fazer
+const Home = (req: Request, res: Response) => {
+    res.end('It\'s a HOME');
+}
+app.get('/home', Home);
+
+// rota dinâmica
+app.get('/noticia/:slug', (req: Request, res: Response) => {
+    let slug =  req.params.slug
+    res.send(`Notícia: ${slug}`);
+});
+
+app.get('/:origin-:target', (req: Request, res: Response) => {
+    let {origin, target} = req.params;
+
+    res.send(`Procurando voos de ${origin.toUpperCase()} para ${target.toUpperCase()}`);
+});
+
+app.listen(port, () => {
+    console.log(`Exemple app listen on port http://localhost:${port}`);
+})
