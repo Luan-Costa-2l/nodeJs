a88658a8c2443b2b689db9280dce506d585a9406
Luan Cordeiro <luam.lc19@gmail.com>
2023-04-22 19:33:58 -0300

    working with mvc part2  models

--- ex010_mvc/src/controlers/homeController.ts
+++ ex010_mvc/src/controlers/homeController.ts
@@ -1,11 +1,17 @@
 import { Request, Response } from 'express';
 
+import { Product } from '../models/Product';
 export const home = (req: Request, res: Response) => {
     let user = {
         name: 'Luan',
         age: 90
     }
+
+    let list = Product.getAll();
+    let expensiveList = Product.getfromPriceAfter(20);
     res.render('pages/home', {
         user,
+        products: list,
+        expensives: expensiveList,
     });
 }
--- ex010_mvc/src/models/Product.ts
+++ ex010_mvc/src/models/Product.ts
@@ -8,10 +8,14 @@ const data: Product[] = [
     { title: 'Product X', price: 10 },
     { title: 'Product Y', price: 15 },
     { title: 'Product Z', price: 20 },
+    { title: 'Product X2', price: 25 },
 ];
 
 export const Product = {
     getAll: (): Product[] => {
         return data;
+    },
+    getfromPriceAfter: (price: number): Product[] => {
+        return data.filter(product => product.price >= price);
     }
 };
