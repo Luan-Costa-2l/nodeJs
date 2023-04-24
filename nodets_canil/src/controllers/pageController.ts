8273709ba60af2eae3c07d34a407cfa8da38b749
Luan Cordeiro <luam.lc19@gmail.com>
2023-04-24 20:24:25 -0300

    feat: list pets in controller and show its

--- nodets_canil/src/controllers/pageController.ts
+++ nodets_canil/src/controllers/pageController.ts
@@ -1,42 +1,55 @@
 import { Request, Response } from "express";
 import { creteMenuObject } from '../helpers/createMenuObject';
+import { pet } from "../models/pet";
 
 export const home = (req: Request, res: Response) => {
+    let list = pet.getAll();
+
     res.render('pages/page', {
         menu: creteMenuObject('all'),
         banner: {
             title: 'Todos os animais',
             background: 'allanimals.jpg'
         },
+        list
     });
 }
 
 export const dogs = (req: Request, res: Response) => {
+    let list = pet.getFromType('dog');
+
     res.render('pages/page', {
         menu: creteMenuObject('dog'),
         banner: {
             title: 'Cachorros',
             background: 'banner_dog.jpg'
         },
+        list
     });
 }
 
 export const cats = (req: Request, res: Response) => {
+    let list = pet.getFromType('cat');
+
     res.render('pages/page', {
         menu: creteMenuObject('cat'),
         banner: {
             title: 'Gatos',
             background: 'banner_cat.jpg'
         },
+        list
     });
 }
 
 export const fishes = (req: Request, res: Response) => {
+    let list = pet.getFromType('fish');
+
     res.render('pages/page', {
         menu: creteMenuObject('fish'),
         banner: {
             title: 'Peixes',
             background: 'banner_fish.jpg'
         },
+        list
     });
 }
