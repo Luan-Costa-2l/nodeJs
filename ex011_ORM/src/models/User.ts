4ddf88cefd26450f7a3b20611fc265beda169362
Luan Cordeiro <luam.lc19@gmail.com>
2023-04-27 21:15:32 -0300

    models in sequelize + typescript

---
+++ ex011_ORM/src/models/User.ts
@@ -0,0 +1,30 @@
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../instances/postgresql";
+
+export interface UserInstance extends Model {
+    id: number;
+    name: string;
+    age: number;
+}
+
+export const User = sequelize.define<UserInstance>('User', {
+    id: {
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    age: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 18
+    }
+}, {
+    tableName: 'users',
+    timestamps: false
+});
+
+console.log(User === sequelize.models.User);
