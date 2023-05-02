69628bf184c2cc6f3e3883ab3dc719ca2be4a7e2
Luan Cordeiro <luam.lc19@gmail.com>
2023-05-01 21:14:40 -0300

    sorting results from name and others

--- ex012_sorting_data/src/controllers/homeController.ts
+++ ex012_sorting_data/src/controllers/homeController.ts
@@ -1,8 +1,19 @@
 import { Request, Response } from "express";
 import { User } from '../models/User';
+import { Op } from "sequelize";
 
 export const homePage = async (req: Request, res: Response) => {
-    let users = await User.findAll();
+    let users = await User.findAll({
+        where: {
+            age: {
+                [Op.gte]: 18
+            }
+        },
+        order: [
+            ['age', 'ASC']
+        ]
+    });
+
     res.render('pages/home', {
         users
     });
