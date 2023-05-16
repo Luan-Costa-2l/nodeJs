dc5043c51eccbae296920bc1979cc1117bf6392e
Luan Cordeiro <luam.lc19@gmail.com>
2023-05-16 18:57:24 -0300

    learning cors

--- ex020_node_mod_api/src/server.ts
+++ ex020_node_mod_api/src/server.ts
@@ -2,11 +2,15 @@ import express, { Request, Response } from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
 import apiRoutes from './routes/api';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
-
+app.use(cors({
+    origin: 'https://resttesttest.com',
+    methods: ['get']
+}));
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.urlencoded({ extended: true}));
 
