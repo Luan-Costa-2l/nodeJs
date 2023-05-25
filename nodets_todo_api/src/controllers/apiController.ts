20903d80e92deae118176aeaa04da17e1b2847bc
Luan Cordeiro <luam.lc19@gmail.com>
2023-05-25 18:21:23 -0300

    feat(todo): update task api route

--- nodets_todo_api/src/controllers/apiController.ts
+++ nodets_todo_api/src/controllers/apiController.ts
@@ -9,10 +9,42 @@ export const createTask = async (req: Request, res: Response) => {
         res.status(201);
         res.json({ newTask });
     }
-    res.json({ error: "title cannot be null"})
+    res.json({ error: "title cannot be null" })
 }
 
 export const getTasks = async (req: Request, res: Response) => {
     const tasks = await Task.findAll();
     res.json({ tasks });
+}
+
+export const updateTask = async (req: Request, res: Response) => {
+    const { done, title } = req.body;
+    const id: string = req.params.id;
+    const updatedTask = await Task.findByPk(id);
+
+    if (!updatedTask) {
+        res.status(404);
+        res.json({ error: "Task Not Found" });
+        return;
+    }
+    if (title) {
+        updatedTask.title = title;
+    }
+    if (done) {
+        switch (done.toLowerCase()) {
+            case 'false':
+            case '0':
+                updatedTask.done = false;
+                break;
+            case 'true':
+            case '1':
+                updatedTask.done = true;
+                break;
+            default:
+                res.json({error: "Invalid done value"});
+                break;
+        }
+    }
+    await updatedTask.save();
+    res.json({ updatedTask });
 }
--- nodets_todo_api/src/routes/api.ts
+++ nodets_todo_api/src/routes/api.ts
@@ -5,5 +5,6 @@ const router = Router();
 
 router.post('/tasks', apiController.createTask);
 router.get('/tasks', apiController.getTasks);
+router.put('/tasks/:id', apiController.updateTask);
 
 export default router;
