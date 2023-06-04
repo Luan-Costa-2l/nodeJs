6f8026360d8ffb0de585cb473d7b1fb827dba659
Luan Cordeiro <luam.lc19@gmail.com>
2023-06-04 20:54:27 -0300

    Basic auth

--- ex025_authentication/src/middlewares/auth.ts
+++ ex025_authentication/src/middlewares/auth.ts
@@ -1,9 +1,26 @@
 import { Request, Response, NextFunction } from 'express';
+import { User } from '../models/User';
 export const Auth = {
-    private: (req: Request, res: Response, next: NextFunction) => {
+    private: async (req: Request, res: Response, next: NextFunction) => {
         let succes = false;
 
         // verify if was authorized.
+        if (req.headers.authorization) {
+            const hash = req.headers.authorization.substring(6);
+            const decoded = Buffer.from(hash, 'base64').toString();
+            const data = decoded.split(':');
+
+            if (data.length === 2) {
+                const hasUser = await User.findOne({
+                    where: {
+                        email: data[0],
+                        password: data[1]
+                    }
+                })
+
+                succes = !!hasUser;
+            }
+        }
 
         if (succes) {
             next();
